5dd858e144006b28c7caca0dfbb95469fe37d6cf
Muni Mahesh <153695240+mannasamudrammahesh@users.noreply.github.com>
2025-01-06 06:20:24 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,31 @@
-import type { Metadata } from 'next'
-import { ClerkProvider } from '@clerk/nextjs'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { ClerkProvider } from '@clerk/nextjs';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'MetaMind',
   description: 'MetaMind is a platform for mental health awareness and support.',
   icons: {
-    icon: 'public/favicon.ico',
+    icon: '/favicon.ico', // Path to the favicon in the public/ directory
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          {/* Any additional head content */}
+        </head>
         <body className={`${inter.className} backg`}>{children}</body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
