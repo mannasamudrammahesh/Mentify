89c959467ad75f50859d76cb1fe5d025546ccb13
Muni Mahesh <153695240+mannasamudrammahesh@users.noreply.github.com>
2025-01-11 11:29:15 +0530

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,26 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
-
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-export async function POST(req: NextRequest) {
+const genAI = new GoogleGenerativeAI("AIzaSyAx34o31vs5bNBpR8BbftYHU-hC4jqOOJQ");  // API key included
+const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+const chat = model.startChat({
+  history: [],
+  generationConfig: {
+    maxOutputTokens: 1000,
+  },
+});
 
-  // get prompt field from the request body
+export async function POST(req: NextRequest) {
   const reqBody = await req.json();
   const { userPrompt } = reqBody;
-  const prompt = "you are a mental health professional. you are talking to a patient who is suffering from depression. you want to help them feel better. so answer the following questions: " + userPrompt;
-  const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY as string);
-  const model = genAI.getGenerativeModel({ model: "gemini-pro", generationConfig: { maxOutputTokens: 400 }});
-
-  try {
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    return NextResponse.json({
-      text
-    });
-  } catch (error) {
-    return NextResponse.json({
-      text: "Unable to process the prompt. Please try again."
-    });
+  const result = await chat.sendMessage(userPrompt);
+  const response = await result.response;
+  const text = response.text();
+  
+  if (text === "") {
+    return NextResponse.json({ text: "Sorry, I don't understand." });
   }
+  
+  return NextResponse.json({ text });
 }
