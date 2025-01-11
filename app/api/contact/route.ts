b69687b8feca8aad6ba94cff298a6f9dce8439a0
Muni Mahesh <153695240+mannasamudrammahesh@users.noreply.github.com>
2025-01-11 11:30:25 +0530

    Update route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -2,38 +2,25 @@ import { NextRequest } from 'next/server';
 import { ContactEmailTemplate } from '@/components/email-template';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_9nSrzkiw_LjaXVAXvcu74HuJGKW7cwN2Y");  
 
 export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const { name, email, message } = body;
 
-    const body = await request.json();
+  if (!name || !email || !message) {
+    return NextResponse.json({ error: 'Missing required fields' });
+  }
 
-    const { name, email, message } = body;
-
-    if (!name) {
-        return Response.json({ error: 'Missing name' });
-    }
-
-    if (!email) {
-        return Response.json({ error: 'Missing email' });
-
-    }
-
-    if (!message) {
-        return Response.json({ error: 'Missing message' });
-    }
-
-    try {
-        // @ts-ignore
-        const data = await resend.emails.send({
-            from: 'Acme <onboarding@resend.dev>',
-            to: process.env.PERSONAL_EMAIL as string,
-            subject: 'Message from Mentify',
-            react: ContactEmailTemplate({ name:name, email: email, message: message }),
-        });
-
-        return Response.json(data);
-    } catch (error) {
-        return Response.json({ error });
-    }
+  try {
+    const data = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: "maheshh130506@gmail.com",
+      subject: 'Message from Mentify',
+      react: ContactEmailTemplate({ name, email, message }),
+    });
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
 }
